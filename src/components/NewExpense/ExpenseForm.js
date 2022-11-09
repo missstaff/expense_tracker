ea888a452b6fd855ce70fa8b9129962acf05b274
missstaff <shawnastaff@gmail.com>
2022-11-08 21:07:07 -0800

    dynamically rendering theadd new expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -34,8 +34,9 @@ const ExpenseForm = (props) => {
     setTitle("");
   };
 
+
   return (
-    <form onSubmit={submitHandler}>
+      <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
@@ -51,7 +52,8 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type="submit">Add New Expense</button>
       </div>
     </form>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 
 import "../../css/NewExpense.css";
 
+
 const NewExpense = (props) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     }
     props.onAddExpense(expenseData);
-  }
+    setIsEditing(!isEditing);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(!isEditing)
+  };
+
   return <div className="new-expense">
-    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    {!isEditing && 
+      <button 
+        onClick={startEditingHandler}>
+          Add New Expense
+      </button>
+    }
+    {isEditing && 
+      <ExpenseForm 
+        onSaveExpenseData={saveExpenseDataHandler} 
+        onCancel={stopEditingHandler} 
+        />
+      }
   </div>
 };
 
